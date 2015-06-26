8dbed8a52843f294815c5467262bcf7cccb433aa
juk85 <untaljuk@digitaldealers.mx>
2015-06-25 21:46:04 -0500

    arreglo-responsive

--- public/js/main.js
+++ public/js/main.js
@@ -21,4 +21,37 @@ $(document).on("ready",
 			var top = $('.contacto').offset().top;
 			TweenLite.to(window, 2, {scrollTo:{y: top}, ease:Power2.easeOut});
 		});
+		$('#form').submit(function(event) {
+		var $form	= $(this);
+		var name 	= $("#form-name").val();
+		var mail 	= $form.find("#form-mail").val();
+		var nameIsString	= /[A-Za-z\ ]+/.test(name);
+		var emailIsValid	= /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/igm.test(email);
+
+		if(!name || !nameIsString){
+			$("#form-name").addClass("rojo");
+			$("form-name").val("");
+			$("#dat-obli").css({display: "block"});
+			return false;
+		}else{
+			$("#dat-obli").css({display: "none"});
+		}
+		if(!email|| !emailIsValid){
+			$("#form-mail").addClass("rojo");
+			$("#form-mail").val("");
+			$(".incorrecto").css({display: "block"});
+			$(".vacio").css({display: "none"});
+			$(".correcto").css({display: "none"});
+			$("#dat-obli").css({display: "block"});
+			return false;
+		}else{
+			$(".correcto").css({display: "block"});
+			$(".vacio").css({display: "none"});
+			$(".incorrecto").css({display: "none"});
+			$("#dat-obli").css({display: "none"});
+		}
+
+		$form.find('#boton-enviar').prop('disabled', true);
+		return false;
+	});
 });
